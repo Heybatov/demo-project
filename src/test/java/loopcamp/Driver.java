7d8a26f5ee965b6ce415ce1d994224e88c23e597
Heybatov <heybatov.ruslan82@gmail.com>
2023-06-03 12:12:10 -0400

    nn

--- src/test/java/loopcamp/Driver.java
+++ src/test/java/loopcamp/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.90.92.120";
+                        String gridAddress = "3.86.144.64";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
@@ -47,7 +47,7 @@ public class Driver {
                 case "remote-firefox":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.90.92.120";
+                        String gridAddress = "3.86.144.64";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("firefox");
